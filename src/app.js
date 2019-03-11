bac867e0c3864ea27f026ec13841dfe46a532c3d
GeoNicol <gnicolaidis@xamplifier.com>
2019-03-11 19:08:01 +0200

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //define paths for express config
 const publicDirectory = path.join(__dirname,'../public')
@@ -79,7 +80,6 @@ app.get('/products', (req,res)=>{
     })
 })
 
-
 app.get('/help/*', (req,res)=>{
     res.render('404',{
         title: '404 Article not found',
@@ -96,6 +96,6 @@ app.get('*', (req,res)=>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port '+port)
 }) 
